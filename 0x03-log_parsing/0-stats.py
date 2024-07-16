898ed52b00f78ac36f95b62c0d6e7fc6ec275d9f
Job  Nayere <37332656+nayere@users.noreply.github.com>
2024-07-16 19:39:16 +0300

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,13 +3,16 @@
 """Script that reads stdin line by line and computes metrics"""
 
 import sys
+
+
 def printsts(dic, size):
     """ WWPrints information """
     print("File size: {:d}".format(size))
     for i in sorted(dic.keys()):
         if dic[i] != 0:
             print("{}: {:d}".format(i, dic[i]))
-          
+
+
 sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
        "404": 0, "405": 0, "500": 0}
 
@@ -28,12 +31,15 @@ try:
             size += int(stlist[-1])
         except:
             pass
+
         try:
             if stlist[-2] in sts:
                 sts[stlist[-2]] += 1
         except:
             pass
     printsts(sts, size)
+
+
 except KeyboardInterrupt:
     printsts(sts, size)
     raise
